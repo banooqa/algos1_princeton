ea70e25a6e1abd61be8305797512b6db1eb9819a
banukah <56572382+banukah@users.noreply.github.com>
2020-04-05 14:01:47 -0500

    Hello World added!

---
+++ w0_helloWorld/HelloWorld.java
@@ -0,0 +1,13 @@
+import edu.princeton.cs.algs4.StdOut;
+
+/******************************************************************************
+ *  Basic Hello World Program
+ *
+ ******************************************************************************/
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        StdOut.println("Hello WORLD!!!");
+    }
+}
